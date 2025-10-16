4b2c2cc6ba9a65a4c006c2d7766ab93816e66401
Holger Hinzberg <holger@hinzberg.de>
2025-10-16 08:46:47 +0200

    Initial Commit

---
+++ Reviewer/ReviewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReviewerApp.swift
+//  Reviewer
+//
+//  Created by Holger Hinzberg on 16.10.25.
+//
+
+import SwiftUI
+
+@main
+struct ReviewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
